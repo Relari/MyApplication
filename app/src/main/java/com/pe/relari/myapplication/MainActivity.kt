16a577c6eef410ea761ff9833f3ba1593c526c48
Renzo Lavado Rivas <renzolavador@gmail.com>
2021-03-21 11:52:46 -0500

    fix: values

--- app/src/main/java/com/pe/relari/myapplication/MainActivity.kt
+++ app/src/main/java/com/pe/relari/myapplication/MainActivity.kt
@@ -4,38 +4,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.pe.relari.myapplication.business.model.api.EmployeeRequest
 import com.pe.relari.myapplication.view.EmployeeView
 
 class MainActivity : AppCompatActivity() {
 
-    var txtName : TextView = findViewById(R.id.txtName)
-    var txtPosition : TextView = findViewById(R.id.txtPosition)
-    var txtSex : TextView = findViewById(R.id.txtSex)
-    var txtSalary : TextView = findViewById(R.id.txtSalary)
-    var btnRegister : Button = findViewById(R.id.btnRegister)
-
-    private val employeeView: EmployeeView = EmployeeView()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val employeeView = EmployeeView()
+
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
         btnRegister.setOnClickListener {
 
-            //employeeView.save(buildEmployeeRequest())
+            val employeeRequest = buildEmployeeRequest()
 
-            employeeView.save2(buildEmployeeRequest())
+//            employeeView.save(employeeRequest)
+            employeeView.save2(employeeRequest)
+
+            Toast.makeText(this, employeeRequest.toString(), Toast.LENGTH_LONG).show()
         }
 
     }
 
     private fun buildEmployeeRequest(): EmployeeRequest {
+
+        val txtName = findViewById<TextView>(R.id.txtName)
+        val txtPosition = findViewById<TextView>(R.id.txtPosition)
+        val txtSex = findViewById<TextView>(R.id.txtSex)
+        val txtSalary = findViewById<TextView>(R.id.txtSalary)
+
         return EmployeeRequest(
-            txtName.getText().toString(),
-            txtPosition.getText().toString(),
-            txtSex.getText().toString(),
-            txtSalary.getText().toString().toDouble())
+            txtName.text.toString(),
+            txtPosition.text.toString(),
+            txtSex.text.toString(),
+            txtSalary.text.toString().toDouble())
 
     }
 }

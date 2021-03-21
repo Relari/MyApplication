947f1bd95f2c3f359d9f73e2f010ee34da599f97
Renzo Lavado Rivas <renzolavador@gmail.com>
2021-03-21 13:42:12 -0500

    refactor: completed method

--- app/src/main/java/com/pe/relari/myapplication/business/dao/impl/EmployeeDaoImpl.kt
+++ app/src/main/java/com/pe/relari/myapplication/business/dao/impl/EmployeeDaoImpl.kt
@@ -1,5 +1,7 @@
 package com.pe.relari.myapplication.business.dao.impl
 
+import android.util.Log
+import com.google.gson.Gson
 import com.pe.relari.myapplication.business.dao.EmployeeDao
 import com.pe.relari.myapplication.business.model.business.Employee
 import com.pe.relari.myapplication.business.model.entity.EmployeeEntity
@@ -8,6 +10,9 @@ import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class EmployeeDaoImpl(private val restConfiguration: RestConfiguration = RestConfiguration()) : EmployeeDao {
 
@@ -36,6 +41,18 @@ class EmployeeDaoImpl(private val restConfiguration: RestConfiguration = RestCon
   override fun save2(employee: Employee) {
       val employeeEntity = mapEmployeeEntity(employee)
       restConfiguration.employeeRepository().save2(employeeEntity)
+          .enqueue(object : Callback<Void> {
+              override fun onFailure(call: Call<Void>?, t: Throwable?) {
+                  // failure
+                  t?.printStackTrace()
+              }
+
+              override fun onResponse(call: Call<Void>?, response: Response<Void>?) {
+                  // success
+                  val isSave = response?.isSuccessful
+                  Log.i("Employee saved", Gson().toJson(MessageResponse(isSave.toString().toBoolean())))
+              }
+          })
   }
 
 
@@ -49,3 +66,5 @@ class EmployeeDaoImpl(private val restConfiguration: RestConfiguration = RestCon
         employee.status)
   }
 }
+
+data class MessageResponse(val msg: Boolean)
